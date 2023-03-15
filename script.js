a8146af0a170283f0e19f1edeb02ba6b94ecc431
SnowStriX <secondex@mail.ru>
2023-03-15 14:10:49 +0300

    main
    main

---
+++ script.js
@@ -0,0 +1,51 @@
+window.addEventListener("scroll", function(){
+      var header = document.querySelector("header",);
+      header.classList.toggle("active",window.scrollY > 0);
+    });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  effect: 'fade',
+
+  spaceBetween: 20,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    dynamicBullets: true,
+  },
+});
+
+const toggleBtn = document.querySelector('.toggle_btn')
+    const toggleBtnIcon = document.querySelector('.toggle_btn i')
+    const DropDownMenu = document.querySelector('.dropdown_menu')
+
+    toggleBtn.onclick = function () {
+      DropDownMenu.classList.toggle('open')
+      const isOpen = DropDownMenu.classList.contains('open')
+
+      toggleBtnIcon.classList = isOpen
+      ? 'fa-solid fa-xmark'
+      : 'fa-solid fa-bars-staggered'
+    };
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
